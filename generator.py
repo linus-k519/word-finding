076481c44a3ff71bca25cd93a81bcf2e9bb1022c
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2021-02-03 17:48:08 +0100

    Used generator

--- generator.py
+++ generator.py
@@ -1,25 +1,31 @@
 import random
 
-# Read duden textfile
-duden = []
-with open('duden.txt') as file:
-    duden = file.readlines()
-    
-# Remove newlines
-for index, line in enumerate(duden):
-    duden[index] = line[:-1]
 
-# Filter for words of given length
+def random_word(word_len: int, num_words: int) -> str:
+    # Read duden textfile
+    duden = []
+    with open('duden.txt') as file:
+        duden = file.readlines()
+
+    # Remove newlines
+    for index, line in enumerate(duden):
+        duden[index] = line[:-1]
+
+    # Filter for words of given length
+    duden = list(filter(lambda x: len(x) == word_len, duden))
+
+    for i in range(num_words):
+        # Select random element
+        random_number = random.randint(0, len(duden))
+        word = duden[random_number].upper()
+
+        # Disorder word
+        disordered_word = ''.join(random.sample(word, len(word)))
+        yield disordered_word
+
+
 word_len = int(input('Word length: '))
-duden = list(filter(lambda x: len(x) == word_len, duden))
-
 num_words = int(input('Number of words: '))
 
-for i in range(num_words):
-    # Select random element
-    random_number = random.randint(0, len(duden))
-    word = duden[random_number].upper()
-
-    # Disorder word
-    disordered_word = ''.join(random.sample(word, len(word)))
-    print(disordered_word, ' -> ', word)
+for w in random_word(word_len, num_words):
+    print(w)
